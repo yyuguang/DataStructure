52cc95d07ee17ac34a54ec01190a5cec40787799
yyuguang <790335255@qq.com>
2020-02-05 11:29:14 +0800

    数据结构-双向链表

---
+++ src/com/lnzz/linkedlist/doubly/DoubleLinkedList.java
@@ -0,0 +1,180 @@
+package com.lnzz.linkedlist.doubly;
+
+import com.lnzz.linkedlist.AbstractList;
+
+/**
+ * ClassName：DoubleLinkedList
+ *
+ * @author 冷暖自知
+ * @version 1.0
+ * @date 2020/2/1 10:56
+ * @Description: 双向链表
+ */
+public class DoubleLinkedList<E> extends AbstractList<E> {
+
+    private Node<E> first;
+    private Node<E> last;
+
+    private static class Node<E> {
+        E element;
+        Node<E> prev;
+        Node<E> next;
+
+        public Node(Node<E> prev, E element, Node<E> next) {
+            this.prev = prev;
+            this.element = element;
+            this.next = next;
+        }
+
+        @Override
+        protected void finalize() throws Throwable {
+            System.out.println("Node - finalize");
+        }
+
+        @Override
+        public String toString() {
+
+            StringBuilder sb = new StringBuilder();
+            if (prev != null) {
+                sb.append(prev.element);
+            } else {
+                sb.append("null");
+            }
+            sb.append("_").append(element).append("_");
+            if (next != null) {
+                sb.append(next.element);
+            } else {
+                sb.append("null");
+            }
+            return sb.toString();
+        }
+    }
+
+    public void clear() {
+        size = 0;
+        first = null;
+        last = null;
+    }
+
+
+    public E get(int index) {
+        return node(index).element;
+    }
+
+    public E set(int index, E element) {
+        Node<E> node = node(index);
+        E old = node.element;
+        node.element = element;
+        return old;
+    }
+
+    public void add(int index, E element) {
+        rangeCheckForAdd(index);
+        if (index == size) {
+            Node<E> oldLast = last;
+            last = new Node<E>(oldLast, element, null);
+            if (oldLast == null) {
+                first = last;
+            } else {
+                oldLast.next = last;
+            }
+        } else {
+            Node<E> next = node(index);
+            Node<E> prev = next.prev;
+            Node<E> node = new Node<E>(prev, element, next);
+            next.prev = node;
+            if (prev == null) {
+                first = node;
+            } else {
+                prev.next = node;
+            }
+        }
+        size++;
+    }
+
+    public E remove(int index) {
+        rangeCheck(index);
+        Node<E> node = node(index);
+        Node<E> prev = node.prev;
+        Node<E> next = node.next;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+        }
+
+        size--;
+        return node.element;
+    }
+
+    public int indexOf(E element) {
+        if (element == null) {
+            Node<E> node = first;
+            for (int i = 0; i < size; i++) {
+                if (node.element == null) {
+                    return i;
+                }
+                node = node.next;
+            }
+        } else {
+            Node<E> node = first;
+            for (int i = 0; i < size; i++) {
+                if (element.equals(node.element)) {
+                    return i;
+                }
+                node = node.next;
+            }
+        }
+        return ELEMENT_NOT_FOUND;
+    }
+
+
+    /**
+     * 获取index位置对应的节点对象
+     *
+     * @param index
+     * @return
+     */
+    private Node<E> node(int index) {
+        rangeCheck(index);
+
+        if (index < (size >> 1)) {
+            Node<E> node = first;
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+            return node;
+        } else {
+            Node<E> node = last;
+            for (int i = size - 1; i > index; i--) {
+                node = node.prev;
+            }
+            return node;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("size=").append(size).append(", [");
+        Node<E> node = first;
+        for (int i = 0; i < size; i++) {
+
+            if (i != 0) {
+                stringBuilder.append(",");
+            }
+            stringBuilder.append(node);
+            node = node.next;
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+
+}
