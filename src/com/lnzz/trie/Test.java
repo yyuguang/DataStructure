152e602ee1411bdf9a2d2a4bde88acb1187ece6c
yyuguang <790335255@qq.com>
2020-03-03 10:48:08 +0800

    数据结构 - Trie

---
+++ src/com/lnzz/trie/Test.java
@@ -0,0 +1,37 @@
+package com.lnzz.trie;
+
+import com.lnzz.util.Asserts;
+
+/**
+ * ClassName：Test
+ *
+ * @author 冷暖自知
+ * @version 1.0
+ * @date 2020/3/1 14:49
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        Trie<Integer> trie = new Trie<>();
+        trie.add("cat", 1);
+        trie.add("dog", 2);
+        trie.add("catalog", 3);
+        trie.add("cast", 4);
+        trie.add("冷暖自知", 5);
+        Asserts.test(trie.size() == 5);
+        Asserts.test(trie.startsWith("do"));
+        Asserts.test(trie.startsWith("c"));
+        Asserts.test(trie.startsWith("ca"));
+        Asserts.test(trie.startsWith("cat"));
+        Asserts.test(trie.startsWith("cata"));
+        Asserts.test(!trie.startsWith("hehe"));
+        Asserts.test(trie.get("冷暖自知") == 5);
+        Asserts.test(trie.remove("cat") == 1);
+        Asserts.test(trie.remove("catalog") == 3);
+        Asserts.test(trie.remove("cast") == 4);
+        Asserts.test(trie.size() == 2);
+//        Asserts.test(trie.startsWith("小"));
+        Asserts.test(trie.startsWith("do"));
+        Asserts.test(!trie.startsWith("c"));
+    }
+}
